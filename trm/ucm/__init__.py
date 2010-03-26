7b06af61b3311b446d72a9847d78a4fa9d1db042
Tom Marsh <t.r.marsh@warwick.ac.uk>
2010-03-26 17:33:41 +0000

    Updates for new data type
    Have added unsigned short ints to possible ultracam header items so this needed updating. Also there was a bug in the PGPLOT part and I moved a routine from subs into its more natural home here.

--- trm/ucm/__init__.py
+++ trm/ucm/__init__.py
@@ -3,16 +3,17 @@
 """
 enables python access to ultracam ucm files
 
-ULTRACAM stores frames in its own native binary format, which present a
+ULTRACAM stores frames in its own native binary format, which presents a
 high hurdle for one-off manipulations. This module allows you to read 
 ULTRACAM frames into python where you can use standard python code to do 
 what you want to the data and headers.
 
 The headers are stored in an ordered dictionary keyed on the header parameter
 name. Each parameter has a value, a comment and a type. The types refer to 
-particular C++ data types and you are better off not changing them if possible.
+particular C++ data types and you are better off not changing them if possible
+(however, read further below if you want to).
 
-example:
+Example:
 
 import trm.ucm
 
@@ -25,6 +26,18 @@ ucm[2][1] -= 10.
 # dump result
 ucm.write('image_new.ucm')
 
+Headers:
+
+The Ucm class is inherited from trm.subs.Odict, so you could set a header item as
+follows:
+
+ucm['My.Header.Item'] = {'value' : 23.345, 'comment' : 'a test header item', 'type' : trm.ucm.ITYPE_DOUBLE}
+
+The last one tells it what C++ data type to use; see below for a full list but note that not
+all of them have been implemented. The times are a little subtle. You need to provide a 2 element tuple
+with an integer MJD day number and a double representing the hour of the day and this is what you will
+get back if you query a time header parameter (ITYPE_TIME)
+
 
 Classes
 =======
@@ -34,7 +47,8 @@ Ucm     -- class to contain a ucm frame (multiple windows of multiple CCDs)
 Functions
 =========
 
-rucm    -- read a ucm file and create a Ucm object
+open_ucm  -- opens a ucm file, works out endianness
+rucm      -- read a ucm file and create a Ucm object
 
 Dependencies
 ============
@@ -50,6 +64,48 @@ import ppgplot
 import trm.subs as subs
 import trm.subs.cpp as cpp
 
+# Integer type numbers
+ITYPE_DOUBLE    = 0
+ITYPE_CHAR      = 1
+ITYPE_INT       = 2
+ITYPE_UINT      = 3
+ITYPE_LINT      = 4
+ITYPE_ULINT     = 5
+ITYPE_FLOAT     = 6
+ITYPE_STRING    = 7
+ITYPE_BOOL      = 8
+ITYPE_DIR       = 9
+ITYPE_DATE      = 10
+ITYPE_TIME      = 11
+ITYPE_POSITION  = 12
+ITYPE_DVECTOR   = 13
+ITYPE_UCHAR     = 14
+ITYPE_TELESCOPE = 15
+ITYPE_USINT     = 16
+
+def open_ucm(fname):
+    """
+    Opens a ucm file for reading and returns a file object
+
+    Returns (fobj,endian) where fobj is the file object and endian is a string to be passed
+    to later routines indicating endian-ness
+    """
+    MAGIC = 47561009
+    fobj = open(fname, 'rb')
+
+    # read the format code
+    fbytes = fobj.read(4)
+    (fcode,) = struct.unpack('i',fbytes)
+    if fcode != MAGIC:
+        (fcode,) = struct.unpack('>i',fbytes)
+        if fcode != MAGIC:
+            fobj.close()
+            raise CppError('open_ucm: could not recognise first 4 bytes of ' + fname + ' as a ucm file')
+        endian = '>'
+    else:
+        endian = ''
+    return (fobj,endian)
+
 class Ucm(subs.Odict):
 
     """
@@ -57,14 +113,14 @@ class Ucm(subs.Odict):
 
     Ucm is a sub-class of an Odict (ordered dictionary). The Odict is used to store
     the header while extra attributes store the data in numpy 2D arrays. The Odict is keyed
-    on the header item name with '.' used to descend into directory hierarchys. So for instance,
+    on the header item name with '.' used to descend into directory hierarchies. So for instance,
     if you read a file as follows:
 
     flat = trm.ucm.rucm('flat.ucm')
     
     then
 
-    flat['Site.Observatory'['value']
+    flat['Site.Observatory']['value']
 
     will return the observatory name.
 
@@ -131,39 +187,43 @@ class Ucm(subs.Odict):
             uf.write(struct.pack('i',itype))
             cpp.write_string(uf, val['comment'])
 
-            if itype == 0: # double
+            if itype == ITYPE_DOUBLE: # double
                 uf.write(struct.pack('d', val['value']))
-            elif itype == 1: # char
+            elif itype == ITYPE_CHAR: # char
                 raise Exception('Hitem: char not enabled')
-            elif itype == 2: # int
+            elif itype == ITYPE_INT: # int
                 uf.write(struct.pack('i', val['value']))
-            elif itype == 3: # uint
+            elif itype == ITYPE_UINT: # uint
                 raise Exception('Hitem: uint not enabled')
-            elif itype == 4: # lint
+            elif itype == ITYPE_LINT: # lint
                 raise Exception('Hitem: linit not enabled')
-            elif itype == 5: # ulint
+            elif itype == ITYPE_ULINT: # ulint
                 raise Exception('Hitem: ulint not enabled')
-            elif itype == 6: # float
+            elif itype == ITYPE_FLOAT: # float
                 uf.write(struct.pack('f', val['value']))
-            elif itype == 7: # string
+            elif itype == ITYPE_STRING: # string
                 cpp.write_string(uf, val['value'])
-            elif itype == 8: # bool
+            elif itype == ITYPE_BOOL: # bool
                 uf.write(struct.pack('B', val['value']))
-            elif itype == 9: # directory
+            elif itype == ITYPE_DIR: # directory
                 pass
-            elif itype == 10: # date
+            elif itype == ITYPE_DATE: # date
                 raise Exception('Hitem: date not enabled')
-            elif itype == 11: # time
+            elif itype == ITYPE_TIME: # time
                 uf.write(struct.pack('i', val['value'][0]))
                 uf.write(struct.pack('d', val['value'][1]))
-            elif itype == 12: # position
+            elif itype == ITYPE_POSITION: # position
                 raise Exception('Hitem: position not enabled')
-            elif itype == 13: # dvector
+            elif itype == ITYPE_DVECTOR: # dvector
                 raise Exception('Hitem: dvector not enabled')
-            elif itype == 14: # uchar
+            elif itype == ITYPE_UCHAR: # uchar
                 uf.write(struct.pack('c', val['value']))
-            elif itype == 15: # telescope
+            elif itype == ITYPE_TELESCOPE: # telescope
                 raise Exception('Hitem: telescope not enabled')
+            elif itype == ITYPE_USINT: # unsigned short int
+                uf.write(struct.pack('H', val['value']))
+            else:
+                raise Exception('Hitem: type =' + str(itype) + 'not recognised')
 
 # number of CCDs
         nccd = len(self.data)
@@ -231,17 +291,17 @@ class Ucm(subs.Odict):
             tr[3] = self.off[nccd][nw][1]-1
             tr[4] = 0.
             tr[5] = self.ybin
-            ppgplot.pggray(self.data[nccd][nw], nx, ny, 0, nx-1, 0, ny-1, imin, imax, tr)
+            ppgplot.pggray(self.data[nccd][nw], 0, nx-1, 0, ny-1, imin, imax, tr)
 
 
 def rucm(fname):
     """
     Read from disk in ucm format
 
-    fname  -- file to write to.
+    fname  -- file to read from
     """    
 
-    (uf,start_format) = cpp.open_ucm(fname)
+    (uf,start_format) = open_ucm(fname)
 
 # read the header
     (lmap,) = struct.unpack(start_format + 'i', uf.read(4))
@@ -252,40 +312,42 @@ def rucm(fname):
         (itype,) = struct.unpack(start_format + 'i', uf.read(4))
         comment = cpp.read_string(uf, start_format)
 
-        if itype == 0: # double
+        if itype == ITYPE_DOUBLE: # double
             (value,) = struct.unpack(start_format + 'd', uf.read(8))
-        elif itype == 1: # char
+        elif itype == ITYPE_CHAR: # char
             raise Exception('Hitem: char not enabled')
-        elif itype == 2: # int
+        elif itype == ITYPE_INT: # int
             (value,) = struct.unpack(start_format + 'i', uf.read(4))
-        elif itype == 3: # uint
+        elif itype == ITYPE_UINT: # uint
             raise Exception('Hitem: uint not enabled')
-        elif itype == 4: # lint
+        elif itype == ITYPE_LINT: # lint
             raise Exception('Hitem: linit not enabled')
-        elif itype == 5: # ulint
+        elif itype == ITYPE_ULINT: # ulint
             raise Exception('Hitem: ulint not enabled')
-        elif itype == 6: # float
+        elif itype == ITYPE_FLOAT: # float
             (value,) = struct.unpack(start_format + 'f', uf.read(4))
-        elif itype == 7: # string
+        elif itype == ITYPE_STRING: # string
             value = cpp.read_string(uf, start_format)
-        elif itype == 8: # bool
+        elif itype == ITYPE_BOOL: # bool
             (value,) = struct.unpack(start_format + 'B', uf.read(1))
-        elif itype == 9: # directory
+        elif itype == ITYPE_DIR: # directory
             value = None
-        elif itype == 10: # date
+        elif itype == ITYPE_DATE: # date
             raise Exception('Hitem: date not enabled')
-        elif itype == 11: # time
+        elif itype == ITYPE_TIME: # time
             (mjd,)  = struct.unpack(start_format + 'i', uf.read(4))
             (hour,) = struct.unpack(start_format + 'd', uf.read(8))
             value   = (mjd, hour)
-        elif itype == 12: # position
+        elif itype == ITYPE_POSITION: # position
             raise Exception('Hitem: position not enabled')
-        elif itype == 13: # dvector
+        elif itype == ITYPE_DVECTOR: # dvector
             raise Exception('Hitem: dvector not enabled')
-        elif itype == 14: # uchar
+        elif itype == ITYPE_UCHAR: # uchar
             (value,) = struct.unpack(start_format + 'c', uf.read(1))
-        elif itype == 15: # telescope
+        elif itype == ITYPE_TELESCOPE: # telescope
             raise Exception('Hitem: telescope not enabled')
+        elif itype == ITYPE_USINT: # unsigned short int
+            (value,) = struct.unpack(start_format + 'H', uf.read(2))
 
 # store header information
         head[name] = {'value' : value, 'comment' : comment, 'type' : itype}
